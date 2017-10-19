bc225de4ae71116c964276d553a834926086dc10
ZHANG Chenglong <chenglong.zhang@outlook.com>
2017-10-19 22:25:29 +1300

    Initial Commit

---
+++ TopMovies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TopMovies
+//
+//  Created by ZHANG Chenglong on 19/10/17.
+//  Copyright © 2017 ZHANG Chenglong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
